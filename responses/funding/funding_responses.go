15695c443528487628e4833589ad92fbe577882a
jerry <czw_yeah@hotmail.com>
2022-10-17 11:27:48 +0800

    edit response struct

--- responses/funding/funding_responses.go
+++ responses/funding/funding_responses.go
@@ -2,48 +2,67 @@ package funding
 
 import (
 	models "github.com/jerrychan807/okex/models/funding"
-	"github.com/jerrychan807/okex/responses"
 )
 
 type (
 	GetCurrencies struct {
-		responses.Basic
+		//responses.Basic
+		Code       string `json:"code"`
+		Msg        string
 		Currencies []*models.Currency `json:"data"`
 	}
 	GetBalance struct {
-		responses.Basic
+		//responses.Basic
+		Code     string `json:"code"`
+		Msg      string
 		Balances []*models.Balance `json:"data"`
 	}
 	FundsTransfer struct {
-		responses.Basic
+		//responses.Basic
+		Code      string `json:"code"`
+		Msg       string
 		Transfers []*models.Transfer `json:"data"`
 	}
 	AssetBillsDetails struct {
-		responses.Basic
+		//responses.Basic
+		Code  string `json:"code"`
+		Msg   string
 		Bills []*models.Bill `json:"data"`
 	}
 	GetDepositAddress struct {
-		responses.Basic
+		//responses.Basic
+		Code             string `json:"code"`
+		Msg              string
 		DepositAddresses []*models.DepositAddress `json:"data"`
 	}
 	GetDepositHistory struct {
-		responses.Basic
+		//responses.Basic
+		Code             string `json:"code"`
+		Msg              string
 		DepositHistories []*models.DepositHistory `json:"data"`
 	}
 	Withdrawal struct {
-		responses.Basic
+		//responses.Basic
+		Code        string `json:"code"`
+		Msg         string
 		Withdrawals []*models.Withdrawal `json:"data"`
 	}
 	GetWithdrawalHistory struct {
-		responses.Basic
+		//responses.Basic
+		Code                string `json:"code"`
+		Msg                 string
 		WithdrawalHistories []*models.WithdrawalHistory `json:"data"`
 	}
 	PiggyBankPurchaseRedemption struct {
-		responses.Basic
+		//responses.Basic
+		Code       string `json:"code"`
+		Msg        string
 		PiggyBanks []*models.PiggyBank `json:"data"`
 	}
 	GetPiggyBankBalance struct {
-		responses.Basic
+		//responses.Basic
+		Code     string `json:"code"`
+		Msg      string
 		Balances []*models.PiggyBankBalance `json:"data"`
 	}
 )
--- responses/sub_account/sub_account_responses.go
+++ responses/sub_account/sub_account_responses.go
@@ -3,28 +3,37 @@ package sub_account
 import (
 	"github.com/jerrychan807/okex/models/account"
 	models "github.com/jerrychan807/okex/models/subaccount"
-	"github.com/jerrychan807/okex/responses"
 )
 
 type (
 	ViewList struct {
-		responses.Basic
+		//responses.Basic
+		Code        string `json:"code"`
+		Msg         string
 		SubAccounts []*models.SubAccount `json:"data,omitempty"`
 	}
 	APIKey struct {
-		responses.Basic
+		//responses.Basic
+		Code    string `json:"code"`
+		Msg     string
 		APIKeys []*models.APIKey `json:"data,omitempty"`
 	}
 	GetBalance struct {
-		responses.Basic
+		//responses.Basic
+		Code     string `json:"code"`
+		Msg      string
 		Balances []*account.Balance `json:"data,omitempty"`
 	}
 	HistoryTransfer struct {
-		responses.Basic
+		//responses.Basic
+		Code             string `json:"code"`
+		Msg              string
 		HistoryTransfers []*models.HistoryTransfer `json:"data,omitempty"`
 	}
 	ManageTransfer struct {
-		responses.Basic
+		//responses.Basic
+		Code      string `json:"code"`
+		Msg       string
 		Transfers []*models.Transfer `json:"data,omitempty"`
 	}
 )
